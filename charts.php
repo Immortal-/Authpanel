b0b4bcb0c1c308da38cdd9bea7a6dd96e5d245a5
Chris <immortal-@live.com>
2013-08-11 19:18:08 -0400

    charts.php released 8/11/2013

---
+++ charts.php
@@ -0,0 +1,23 @@
+<?.
+/*
+ * AuthPanel's Charts
+* Coded by Immortal
+* Credits: Twitter's Bootstrap, Hosted 3rd party with a cdn
+* _  PasteBin I stole their Highlighting script and used it in source.php *Sorry pastebin I love you guys*
+* _  8/11/2013
+* */
+  require_once 'globle.php';
+	
+	// Register and throw up ur images here.
+?>
+<html>
+  <head>
+  </head>
+  <body>
+    <div aligh="center">.
+   		<img src= "http://www.freeonlineusers.com/login/generated/dk6v0mtb1x-daily-1481392.png"></img> <br />
+   		<img src= "http://www.freeonlineusers.com/login/generated/dk6v0mtb1x-hourly-1481392.png"></img>
+    	
+    </div>
+  </body>
+</html>
