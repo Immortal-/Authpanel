434cf1a6a05be66602af7e3fa20fa61b8ea1f71a
Immortal- <chris@fbi.al>
2013-08-30 01:10:35 -0400

    Updated to Authcode v1.0.3

--- index.php
+++ index.php
@@ -1,74 +1,27 @@
-<?
-/*
- * AuthPanel's Index
- * Coded by Immortal
- * Credits: Twitter's Bootstrap, Hosted 3rd party with a cdn
- * _  PasteBin I stole their Highlighting script and used it in source.php *Sorry pastebin I love you guys*
- * _ jQuery hosted officaly
- * _  8/11/2013
- * */
-  require_once ('./includes/config.php');
-	
-?>
-
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Auth</title>
+    <title>AuthPanel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+
   </head>
   <body>
-    <!-- Static navbar -->
-    <div class="navbar navbar-static-top">
       <div class="container">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Auth Panel</a>
-        <div class="nav-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="./make.php">Make AuthCode</a></li>
-            </ul>
-            
-             <ul class="nav navbar-nav pull-right">
-            	<li><a class="disabled" href="<? echo ApiLocation;?>">Api Link: <? echo ApiLocation; ?></a></li>
-          	</ul>
-          
-        </div><!--/.nav-collapse -->
+        <a href="http://dl.authpanel.pw"><img style="position: absolute; top: 0.1; left: 0.1; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a> 
+        <div class="jumbotron" style="box-shadow: -25px 25px 35px #888888;">
+            <span class="lead">&nbsp;&nbsp;&nbsp;&nbsp;Using our library!</span>
+            <h5><script src="https://gist.github.com/Immortal-/6386123.js"></script></script></h5>
+        </div>
+        <div align ="center">
+          <h6>ver. 1.0.3</h6>
+        </div>
       </div>
-    </div>
-
-
-    <div class="container">
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Code example</h1>
-        <p>This is a full example on how to use auth panel's api to validate a authcode. <br />
-        I have also included the function to get a persons username based on their authcode.
-         </p>
-        <p>
-             <iframe src="./source2.php" style="border:none;width:100%">
-               <script src="./source.php"></script>
-             </iframe>
-            
-       	 </p>
-        <p>
-          <a class="btn btn-large btn-primary" href="http://pastebin.com/raw.php?i=SBjHFHQp">Download Code &raquo;</a>
-        </p>
-      </div>
-
-    </div> <!-- /container -->
-    
-
-    <!-- JavaScript plugins (requires jQuery) -->
-    <script src="http://code.jquery.com/jquery.js"></script>
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-   <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.js"></script>
-   <script src="https://raw.github.com/scottjehl/Respond/master/respond.min.js" ></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
   </body>
 </html>
